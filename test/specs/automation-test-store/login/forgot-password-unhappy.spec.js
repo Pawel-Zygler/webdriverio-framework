32fbb7fcc73f1ec49d594fdee9c72d56d116c302
zygler.pawel@protonmail.com <zygler.pawel@protonmail.com>
2023-06-15 16:21:21 +0200

    added forgot login unhappy commands support

--- test/specs/automation-test-store/login/forgot-password-unhappy.spec.js
+++ test/specs/automation-test-store/login/forgot-password-unhappy.spec.js
@@ -10,12 +10,12 @@ import commands from "../../../../utils/commands";
 describe("FORGOT PASSWORD PAGE - unhappy path", () => {
   beforeEach(() => {
     HomePage.open();
-    TopMenuComp.loginOrRegister.click();
-    LoginPage.forgotPasswordButton.click();
+    commands.waitThenClick(TopMenuComp.loginOrRegister);
+    commands.waitThenClick(LoginPage.forgotPasswordButton);
   });
 
   it("checks if user is on forgot password page", async () => {
-    await expect(await ForgotPasswordPage.forgotPasswordHeader).toHaveText(
+    await expect(commands.waitThenGetText(ForgotPasswordPage.forgotPasswordHeader)).toHaveText(
       "FORGOT YOUR PASSWORD?"
     );
   });
@@ -48,69 +48,17 @@ describe("FORGOT PASSWORD PAGE - unhappy path", () => {
     expectedErrorMessage,
   } of testCases) {
     it(description, async () => {
-      await ForgotPasswordPage.forgotPasswordLoginName.setValue(loginName);
-      await ForgotPasswordPage.forgotPasswordEmail.setValue(email);
+      await commands.waitThenSetValue(ForgotPasswordPage.forgotPasswordLoginName, loginName);
+      await commands.waitThenSetValue(ForgotPasswordPage.forgotPasswordEmail, email);
       await commands.waitThenClick(await SharedPageComponents.continueButton);
 
       const errorElement = await RegisterPage.validationMessageAboveForm(
         expectedErrorMessage
       );
-      await expect(await errorElement).toHaveTextContaining(
+      
+      await expect(await commands.waitThenGetText(errorElement)).toHaveTextContaining(
         expectedErrorMessage
       );
     });
   }
-
-  //to poniej zamienione na to wyzej wciaz musze sie nauczyc dobrze robic foor loop
-  //chciałbym widzieć powtarzające się linie kodu i uzyc funkcji pomocniczychc ktore wykonaja te kroki
-  //jesli mam wiele testow ktore roznia sie tylko danymi testowuymi, to moge uzyc petli do generowania tych testow
-
-  // it("throws error when only login is submitted ", async () => {
-  //   await ForgotPasswordPage.forgotPasswordLoginName.setValue(
-  //     testData.registeredUser.loginName
-  //   );
-
-  //   await commands.waitThenClick(await SharedPageComponents.continueButton);
-
-  //   const errorElement = await RegisterPage.validationMessageAboveForm(
-  //     testData.failedValidationAboveForm.emailNotProvided
-  //   );
-  //   await expect(await errorElement).toHaveTextContaining(
-  //     testData.failedValidationAboveForm.emailNotProvided
-  //   );
-  // });
-
-  // it("throws error when only email is submitted ", async () => {
-  //   await ForgotPasswordPage.forgotPasswordEmail.setValue(
-  //     testData.registeredUser.email
-  //   );
-
-  //   await commands.waitThenClick(await SharedPageComponents.continueButton);
-
-  //   const errorElement = await RegisterPage.validationMessageAboveForm(
-  //     testData.failedValidationAboveForm.noLoginProvided
-  //   );
-  //   await expect(await errorElement).toHaveTextContaining(
-  //     testData.failedValidationAboveForm.noLoginProvided
-  //   );
-  // });
-
-  // it("throws error when records submitted don't match ", async () => {
-  //   await ForgotPasswordPage.forgotPasswordLoginName.setValue(
-  //     testData.registeredUser.loginName
-  //   );
-  //   //password here is intentionally provided for email field, to not match
-  //   await ForgotPasswordPage.forgotPasswordEmail.setValue(
-  //     testData.registeredUser.password
-  //   );
-
-  //   await commands.waitThenClick(await SharedPageComponents.continueButton);
-
-  //   const errorElement = await RegisterPage.validationMessageAboveForm(
-  //     testData.failedValidationAboveForm.noRecordsMatched
-  //   );
-  //   await expect(await errorElement).toHaveTextContaining(
-  //     testData.failedValidationAboveForm.noRecordsMatched
-  //   );
-  // });
 });
