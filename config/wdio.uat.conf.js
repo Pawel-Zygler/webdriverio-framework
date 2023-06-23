17f1a2995827923b6e05660fe98b3c5e8127391c
zygler.pawel@protonmail.com <zygler.pawel@protonmail.com>
2023-06-23 16:50:29 +0200

    removed redundant folders and config files

--- config/wdio.uat.conf.js
+++
@@ -1,9 +0,0 @@
-import { config as baseConfig } from "../wdio.conf";
-
-export const config = Object.assign(baseConfig, {
-  environment: "TEST",
-  email: "autotest1@gmail.com",
-  firstName: "sarah",
-  password: "pass1",
-  baseUrl: "https://webdriveruniversity.com/",
-});
