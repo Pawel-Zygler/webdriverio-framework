38dc7f8cf4d34b2e7984e4d037c3856160346eca
zygler.pawel@protonmail.com <zygler.pawel@protonmail.com>
2023-12-29 19:32:30 +0100

    Fixed object initialisation

--- utils/db_connect.js
+++ utils/db_connect.js
@@ -1,6 +1,6 @@
-import { createConnection } from "mysql";
+import { mysql } from "mysql";
 
-const connection = createConnection({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
